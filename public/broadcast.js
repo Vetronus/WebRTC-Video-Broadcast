884514bfc0f574a0466bde2019f984a0c045a3ee
Parth Sarthee <14sarthi@gmail.com>
2020-05-14 14:50:25 +0530

    Update broadcast.js

--- public/broadcast.js
+++ public/broadcast.js
@@ -2,7 +2,8 @@ const peerConnections = {};
 const config = {
   iceServers: [
     {
-      urls: ["stun:stun.skyway.io:3478"]
+      //urls: ["stun:stun.skyway.io:3478"]
+      urls: ["stun:stun.l.google.com:19302"]
     }
   ]
 };
